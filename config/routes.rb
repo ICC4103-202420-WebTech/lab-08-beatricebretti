e183089cf98fcb5ca96fb44ca646ae9357fcea7a
beatricebretti <bavaldes1@miuandes.cl>
2024-11-01 15:34:10 -0300

    Controllers and Views

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   root 'training_sessions#index'
+  resources :training_sessions, only: [:index, :new, :create, :edit, :update, :destroy]
 end
